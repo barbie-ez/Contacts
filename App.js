bff607c8f0d365b71936d9f56b9670b93f6a92a0
Barbara Ezomo <barbie.ezomo@gmail.com>
2020-05-07 14:59:39 +0100

    added toggle button

--- App.js
+++ App.js
@@ -1,19 +1,54 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  TouchableOpacity,
+} from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+class App extends React.Component {
+  state = {
+    showContacts: false,
+  };
+
+  toggleContacts = () => {
+    this.setState((prevState) => ({ showContacts: !prevState.showContacts }));
+  };
+
+  render() {
+    return (
+      <SafeAreaView style={styles.noticicationBar}>
+        <View style={styles.container}>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => this.toggleContacts()}
+          >
+            <Text>Show Contacts</Text>
+          </TouchableOpacity>
+        </View>
+      </SafeAreaView>
+    );
+  }
 }
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  noticicationBar: {
+    flex: 1,
+    backgroundColor: "#C4C8CC",
+  },
+  button: {
+    alignItems: "center",
+    backgroundColor: "#C4C8CC",
+    padding: 10,
+    margin: 10,
   },
 });
